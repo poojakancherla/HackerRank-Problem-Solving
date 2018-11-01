c0a9b0c5fdd76f2f21833ad9047fc05e64922b9b
poojakancherla <pknq6@mst.edu>
2018-11-01 06:09:28 -0500

    Performed 'd' left rotations on the array, O(N), easy

---
+++ LeftRotation.py
@@ -0,0 +1,21 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+
+    for i in (a[d:] + a[0:d]):
+
+        print(i, end = " ")
